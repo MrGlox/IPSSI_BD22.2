5634e0e8cfd0c979c0dbf973fa4ebfd54f69908b
mrglox <contact@morgan-leroux.com>
2022-09-29 16:12:34 +0200

    add react-query

--- src/containers/Fetch.jsx
+++ src/containers/Fetch.jsx
@@ -1,28 +1,39 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
 export default function Fetch() {
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
+  //   const [isLoading, setLoading] = useState(true);
+  //   const [error, setError] = useState(null);
+  //   const [data, setData] = useState([]);
 
-  async function requestData() {
-    try {
-      const res = await fetch(
-        "https://baconipsum.com/api/?type=meat-and-filler"
-      );
+  //   async function requestData() {
+  //     try {
+  //       const res = await fetch(
+  //         "https://baconipsum.com/api/?type=meat-and-filler"
+  //       );
 
-      setData(await res.json());
-      setLoading(false);
-    } catch (err) {
-      setLoading(false);
-      setError(err);
-      throw err;
-    }
-  }
+  //       const data = await res.json();
 
-  useEffect(() => {
-    requestData();
-  }, []);
+  //       setTimeout(() => {
+  //         setData(data);
+  //         setLoading(false);
+  //       }, 1000);
+  //     } catch (err) {
+  //       setLoading(false);
+  //       setError(err);
+
+  //       throw err;
+  //     }
+  //   }
+
+  //   useEffect(() => {
+  //     requestData();
+  //   }, []);
+
+  const { isLoading, error, data } = useQuery("bacon", () =>
+    fetch("https://baconipsum.com/api/?type=meat-and-filler").then((res) =>
+      res.json()
+    )
+  );
 
   if (isLoading) {
     return "Loading...";
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import reportWebVitals from "./reportWebVitals";
 import Root from "./routes/root";
@@ -14,6 +15,8 @@ import Appart from "./containers/Form";
 import Fetch from "./containers/Fetch";
 
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -46,7 +49,11 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
