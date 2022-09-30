0d6b774d4ef3a609cf0a40763938dd73297469eb
mrglox <contact@morgan-leroux.com>
2022-09-30 10:00:31 +0200

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,59 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <div id="sidebar">
       <h1>
-        <a href="/">App</a>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { fontWeight: "bold" } : undefined
+          }
+          to="/"
+        >
+          App
+        </NavLink>
       </h1>
       <nav>
         <ul>
           <li>
-            <a href="todo">Todo</a>
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? { fontWeight: "bold" } : undefined
+              }
+              to="/todo"
+            >
+              Todo
+            </NavLink>
           </li>
           <li>
-            <a href="apparts">Apparts</a>
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? { fontWeight: "bold" } : undefined
+              }
+              to="/apparts"
+            >
+              Apparts
+            </NavLink>
           </li>
           <li>
-            <a href="clock">Clock</a>
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? { fontWeight: "bold" } : undefined
+              }
+              to="/clock"
+            >
+              Clock
+            </NavLink>
           </li>
           <li>
-            <a href="fetch">Fetch</a>
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? { fontWeight: "bold" } : undefined
+              }
+              to="/fetch"
+            >
+              Fetch
+            </NavLink>
           </li>
         </ul>
       </nav>
